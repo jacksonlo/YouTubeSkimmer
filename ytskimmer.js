90d29c9c9ca5169815b620b81a584e8a8bf0d0c8
Jackson Lo <7jackson.lo@gmail.com>
2016-06-15 22:34:04 -0400

    Added video search. Fixed initial Play/pause bug. Made initial default video to none

--- ytskimmer.js
+++ ytskimmer.js
@@ -9,9 +9,12 @@ var height = getVars['h'] ? getVars['h'] : '390';
 var width = getVars['w'] ? getVars['w'] : '640';
 var quality = getVars['quality'] ? getVars['quality'] : 'default'; /* small, medium, large, hd720, hd1080, highres or default */
 var controls = getVars['controls'] ? getVars['controls'] : 1;
+var v = getVars['v'];
 
 //Create video embed iframes accordingly when ready
 function onYouTubeIframeAPIReady() {
+	if(v === undefined) return;
+	
 	var videoContainer = $("#videos");
 	for(var i = 0; i < n; ++i) {
 		$("#videos").append("<div id='player"+i+"' index='"+i+"'></div>");
@@ -26,58 +29,7 @@ function onYouTubeIframeAPIReady() {
 			}
 		});
 	}
-}
 
-// On player state change method
-var done = false;
-function onPlayerStateChange(event) {
-	if (event.data == YT.PlayerState.PLAYING && !done) {
-		// setTimeout(stopVideo, 6000);
-		done = true;
-	} else if (event.data == YT.PlayerState.ENDED) {
-		//Stop all videos
-		for(var i = 0; i < n; ++i) {
-			//Stop after 2 seconds to account for division
-			setTimeout(stopVideo(players[i]), 2000);
-		}
-		videosReady = 0;
-	} else if (event.data == YT.PlayerState.CUED && !done) {
-		videosReady++;
-		if(videosReady == n) {
-			for(var i = 0; i < n; ++i) {
-				players[i].playVideo();
-			}
-		}
-	}
-}
-function stopVideo(player) {
-	player.stopVideo();
-}
-
-//On player ready method, set's video settings
-function onPlayerReady(event) {
-	event.target.setPlaybackRate(speed); // This is what you're looking for
-	var length = Math.floor(event.target.getDuration()/n);
-	var index = players.indexOf(event.target);
-	event.target.seekTo(length*index, false);
-	event.target.setPlaybackQuality(quality);
-	
-	if(mute) {
-		event.target.mute();
-	}
-
-	event.target.pauseVideo();
-}
-
-function getUrlVars() {
-	var vars = {};
-	var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
-		vars[key] = value;
-	});
-	return vars;
-}
-
-$(document).ready(function() {
 	var muteOn = true;
 
 	//Topbar play-pause button
@@ -137,6 +89,11 @@ $(document).ready(function() {
 	// 	}
 	// });
 
+	//Submit button for new video
+	$("#submit-button").on('click', function() {
+		window.location = 'index.html?v='+$("#video-search").val();
+	});
+
 	//Mute on hover video
 	$(document).on('mouseenter', 'iframe', function() {
 		if(!muteOn) return;
@@ -151,4 +108,52 @@ $(document).ready(function() {
 		var index = $(this).attr('index');
 		players[index].mute();
 	});
-});
+}
+
+// On player state change method
+var done = false;
+function onPlayerStateChange(event) {
+	if (event.data == YT.PlayerState.PLAYING && !done) {
+		// setTimeout(stopVideo, 6000);
+		done = true;
+	} else if (event.data == YT.PlayerState.ENDED) {
+		//Stop all videos
+		for(var i = 0; i < n; ++i) {
+			//Stop after 2 seconds to account for division
+			setTimeout(stopVideo(players[i]), 2000);
+		}
+		videosReady = 0;
+	} else if (event.data == YT.PlayerState.CUED && !done) {
+		videosReady++;
+		if(videosReady == n) {
+			for(var i = 0; i < n; ++i) {
+				players[i].playVideo();
+			}
+		}
+	}
+}
+function stopVideo(player) {
+	player.stopVideo();
+}
+
+//On player ready method, set's video settings
+function onPlayerReady(event) {
+	event.target.setPlaybackRate(speed); // This is what you're looking for
+	var length = Math.floor(event.target.getDuration()/n);
+	var index = players.indexOf(event.target);
+	event.target.seekTo(length*index, false);
+	event.target.setPlaybackQuality(quality);
+	
+	if(mute) {
+		event.target.mute();
+	}
+
+}
+
+function getUrlVars() {
+	var vars = {};
+	var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+		vars[key] = value;
+	});
+	return vars;
+}
